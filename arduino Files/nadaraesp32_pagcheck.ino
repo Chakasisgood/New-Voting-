999ce3de74b26184d2002fe5160433760d07ace9
Chakasisgood <matthewsolar06@gmail.com>
2024-08-29 22:14:47 +0800

    change print lcd in esp32

--- arduino Files/nadaraesp32_pagcheck.ino
+++ arduino Files/nadaraesp32_pagcheck.ino
@@ -1,60 +1,74 @@
-
 #include <Adafruit_Fingerprint.h>
 #include <HardwareSerial.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define WIFI_SSID "Don't Scan Can Harm A CP"
 #define WIFI_PASSWORD "PLDTwifi44647@"
-// #define WIFI_PASSWORD "12345678"
-// #define WIFI_SSID "Galaxy 08"
 
+// Initialize the LCD. Change the address and dimensions as needed.
+LiquidCrystal_I2C lcd(0x27, 16, 2); // 0x27 is the common I2C address for 16x2 LCDs
 
 WiFiServer server(80);
-
-
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial2);
 
-void setup()
-
-{
+void setup() {
   Serial.begin(9600);
   while (!Serial);  // For Yun/Leo/Micro/Zero/...
   delay(5000);
   Serial.println("\n\nAdafruit finger detect test");
+
+  // Initialize the LCD
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
 
   // set the data rate for the sensor serial port
   finger.begin(57600);
   delay(5000);
   if (finger.verifyPassword()) {
     Serial.println("Found fingerprint sensor!");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Found!");
   } else {
     Serial.println("Did not find fingerprint sensor :(");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Sensor Not Found");
     while (1) { delay(5000); }
   }
 
-  
-  // Connecting to wifi
+  // Connecting to WiFi
   Serial.println();
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(WIFI_SSID);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting WiFi...");
 
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi Connected");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected.");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    
-    server.begin();
-
-    
+  server.begin();
 
   Serial.println(F("Reading sensor parameters"));
   finger.getParameters();
@@ -70,38 +84,59 @@ void setup()
 
   if (finger.templateCount == 0) {
     Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
-  }
-  else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("No Fingerprints");
+    lcd.setCursor(0, 1);
+    lcd.print("Enroll Needed");
+  } else {
     Serial.println("Waiting for valid finger...");
-      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Waiting for Finger");
+    lcd.setCursor(0, 1);
+    lcd.print("Templates: ");
+    lcd.print(finger.templateCount);
   }
 }
 
-void loop()                    // run over and over again
-
-{
+void loop() {
   getFingerprintID();
-  delay(5000);            //don't ned to run this at full speed.
+  delay(5000); // don't need to run this at full speed.
 }
-
 
 uint8_t getFingerprintID() {
   uint8_t p = finger.getImage();
   switch (p) {
     case FINGERPRINT_OK:
       Serial.println("Image taken");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Image Taken");
       break;
     case FINGERPRINT_NOFINGER:
       Serial.println("No finger detected");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("No Finger Detected");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
       Serial.println("Communication error");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Comm Error");
       return p;
     case FINGERPRINT_IMAGEFAIL:
       Serial.println("Imaging error");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Image Error");
       return p;
     default:
       Serial.println("Unknown error");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Unknown Error");
       return p;
   }
 
@@ -110,21 +145,39 @@ uint8_t getFingerprintID() {
   switch (p) {
     case FINGERPRINT_OK:
       Serial.println("Image converted");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Image Converted");
       break;
     case FINGERPRINT_IMAGEMESS:
       Serial.println("Image too messy");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Image Messy");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
       Serial.println("Communication error");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Comm Error");
       return p;
     case FINGERPRINT_FEATUREFAIL:
       Serial.println("Could not find fingerprint features");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Feature Error");
       return p;
     case FINGERPRINT_INVALIDIMAGE:
       Serial.println("Could not find fingerprint features");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Invalid Image");
       return p;
     default:
       Serial.println("Unknown error");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Unknown Error");
       return p;
   }
 
@@ -139,28 +192,44 @@ uint8_t getFingerprintID() {
     int httpResponseCode = http.GET();
     if (httpResponseCode > 0) {
       Serial.println("URL opened in browser");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("URL Opened");
     } else {
       Serial.print("Error in sending request: ");
       Serial.println(httpResponseCode);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Error Request");
     }
     http.end();
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
     Serial.println("Communication error");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Comm Error");
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
     Serial.println("Did not find a match");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("No Match Found");
     return p;
   } else {
     Serial.println("Unknown error");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Unknown Error");
     return p;
   }
 
   // found a match!
   Serial.print("Found ID #"); Serial.print(finger.fingerID);
   Serial.print(" with confidence of "); Serial.println(finger.confidence);
-
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Found ID #");
+  lcd.setCursor(0, 1);
+  lcd.print(finger.fingerID);
   return finger.fingerID;
 }
-
-
-
